71ead58e626146a2f542c3d1bb8f49d76ad7562e
engenharia@deltafire.com.br <Dev@4000*>
2023-02-21 20:39:31 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite o valor do alcool: ");
+
+double.TryParse(Console.ReadLine(), out double alcohol);
+
+Console.Write("\nDigite o valor da gasolina: ");
+
+double.TryParse(Console.ReadLine(), out double gas);
+
+var result = Math.Floor((alcohol / gas) * 100);
+
+if (result <= 72)
+{
+    Console.WriteLine($"Vantagem econômica de {result}% a favor do alcool.");
+}
+else
+{
+    Console.WriteLine("Use gasolina");
+}
